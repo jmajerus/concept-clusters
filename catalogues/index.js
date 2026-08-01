5d839615259b90be91e366214518241db52c80e9
John Majerus <contact@john.majerus.us>
2026-07-31 19:21:17 -0500

    Register Dark Patterns catalogue

--- catalogues/index.js
+++ catalogues/index.js
@@ -2,12 +2,14 @@ import gettingStarted from "./getting-started.js";
 import mediaLiteracyCivicReasoning from "./media-literacy-civic-reasoning.js";
 import conceptLenses from "./concept-lenses.js";
 import struggleAgainstDehumanization from "./struggle-against-dehumanization.js";
+import darkPatterns from "./dark-patterns.js";
 
 export const CATALOGUES = [
   gettingStarted,
   mediaLiteracyCivicReasoning,
   conceptLenses,
-  struggleAgainstDehumanization
+  struggleAgainstDehumanization,
+  darkPatterns
 ];
 
 export default CATALOGUES;
